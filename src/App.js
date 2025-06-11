7c23db2b1c04a5657961b6b720c505a8bdfb4256
lucian-xinitrc <cusmirlucian@gmail.com>
2025-06-11 15:55:07 +0000

    Scene Modification, deploy from laptop

--- src/App.js
+++ src/App.js
@@ -44,6 +44,141 @@ function App() {
           }}>
             <p>John - Oh thanks God a place where I can eat and rest</p>
             <div className="marginbox">
+              <button onClick={() => setScene("oldman")}>Next</button>
+            </div>
+          </div>
+        )
+      case "oldman":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/oldman.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+            <p>Old man - welcome to our Pub have the best salmon and chips from this corner of Scotland.</p>
+            <p>John - Sure I would love a portion.</p>
+            <div className="marginbox">
+              <button onClick={() => setScene("salmon")}>Next</button>
+            </div>
+          </div>
+        )
+      case "salmon":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/salmon.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+          <p>John - This salmon is delicious.</p>
+            <p>Old man - You can stay here for tonight and start again tomorrow</p>
+            <p>John - Thank you very much!</p>
+            <div className="marginbox">
+              <button onClick={() => setScene("room")}>Next</button>
+            </div>
+          </div>
+        )
+      case "room":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/room.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+          <p>Old man - Here it is your bedroom sir.</p>
+            <p>Thank you very much for hospitality!</p>
+            <p>Old man - If you need anything don't hesitate to reach me.</p>
+            <div className="marginbox">
+              <button onClick={() => setScene("midnight")}>Next</button>
+            </div>
+          </div>
+        )
+      case "midnight":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/salmon.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+          <p>*men noises*</p>
+            <p>John - The door is locked where is this old man.</p>
+            <p>John - Hellooo</p>
+            <p>*men noises*</p>
+            <p>I should get out of here.2</p>
+            <div className="marginbox">
+              <button onClick={() => setScene("paperonbed")}>Next</button>
+            </div>
+          </div>
+        )
+      case "paperonbed":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/salmon.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+          <p>John - whats that.</p>
+            <p>*John brings the paper.*</p>
+            <p>I should get out of here.2</p>
+            <div className="marginbox">
+              <button onClick={() => setScene("paper")}>Next</button>
+            </div>
+          </div>
+        )
+      case "paper":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/salmon.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+          <h2>Recipes</h2>
+          <p>The recipe is simple. I stuck in the pan a little, I know how to fry this.</p>
+          <ul>
+          <li>1 The water goes to the way.</li>
+          <li>2 The stars unlock the numerical unknown.</li>
+          <li>3 The knowledge sets you free.</li>
+          </ul>
+            <div className="marginbox">
+              <button>Next</button>
+            </div>
+          </div>
+        )
+      case "action table":
+        return (
+          <div className="scene_background" style={{
+            backgroundImage: "url('/images/salmon.png')", 
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            display: "flex"
+          }}>
+          <h2>Recipes</h2>
+          <p>The recipe is simple. I stuck in the pan a little, I know how to fry this.</p>
+          <ul>
+          <li>1 The water goes to the way.</li>
+          <li>2 The stars unlock the numerical unknown.</li>
+          <li>3 The knowledge sets you free.</li>
+          </ul>
+            <div className="marginbox">
               <button>Next</button>
             </div>
           </div>
